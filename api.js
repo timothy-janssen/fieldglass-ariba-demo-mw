3cefc8eb566b1f7b5ccd97ccf5e50162e8da9bed
Timothy Janssen <timothy.janssen@sap.com>
2019-04-17 14:09:05 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,38 @@
 var request = require('request-promise');
 
 exports.call_api_worker_req_create = function(){	
+	var post_options = {
+	    uri:    "https://jsonplaceholder.typicode.com/posts", // dummy call
+	    method:  "POST",
+	    json:    true,
+	    body: {}
+	};
 
-	return request.post()
-	.then(console.log('[POST] Request completed'))
+	return request.post(post_options)
+	.then( function(data) {
+		console.log('[POST] Request completed')
+		data = {};
+		data.submit_qr = {
+      		"type": "quickReplies",
+      		"content": {
+      		  "title": "Okay. Would you like to submit this requisition request",
+      		  "buttons": [
+      		    {
+      		      "value": "Submit",
+      		      "title": "Submit"
+      		    },
+      		    {
+      		      "value": "Cancel",
+      		      "title": "Cancel"
+      		    },
+      		    {
+      		      "value": "Make Changes",
+      		      "title": "Make Changes"
+      		    }
+      		  ]
+      	}};
+      	return data;
+	})
 	.catch(function (err) {
 		//console.log(err);
 	});
@@ -19,40 +48,70 @@ exports.call_api_catalog_search = function(){
 	};
 
 	return request.post(post_options)
-	.then( function(datd) {
+	.then( function(data) {
 		console.log('[POST] Request completed')
 		data = {};
 		data.catalog_list_data = [{
+			"type": "text",
+			"content": "Here's what I found for catalogs with query phrase MacBook:"
+		},{
     	  "type": "list",
     	  "content": {
-    	  	"title" : "Which laptop would you like to request?"
+    	  	"title" : "Catalog List"
     	    "elements": [{
-    	        "title": "Lenovo T560",
+    	        "title": "Apple MacBook Pro 15\"",
     	        "imageUrl": "",
-    	        "subtitle": "i5 / 16 GB / 512 GB / Black",
+    	        "subtitle": "2399 USD",
     	        "buttons": [{
     	            "value": "Buy the first item",
-    	            "title": "$2,096.10",
+    	            "title": "Order",
     	            "type": "postback"
-    	        }]
+    	        }],
+    	        "details": {
+    	        	"Supplier Name": "REDINGTON DISTRIBUTION PTE LTD",
+    	        	"Supplier Part ID": "AD2561",
+    	        	"Manufacturer Name": "Apple",
+    	        	"Lead Time": "3",
+    	        	"Description": "Apple MacBook Pro with 15 inch Retina display - Laptop - 3.2 GHz flash storage 2880 x 1880 - 802.11ac",
+    	        	"Price": "2399",
+    	        	"Currency": "USD"
+    	        }
     	      },{
     	        "title": "Apple MacBook Pro 13\"",
     	        "imageUrl": "",
-    	        "subtitle": "i5 / 16 GB / 512 GB / Gray",
+    	        "subtitle": "1299 USD",
     	        "buttons": [{
     	            "value": "Buy the second item",
-    	            "title": "$35,000",
+    	            "title": "Order",
     	            "type": "postback"
-    	        }]
+    	        }],
+    	        "details": {
+    	        	"Supplier Name": "REDINGTON DISTRIBUTION PTE LTD",
+    	        	"Supplier Part ID": "AD2841",
+    	        	"Manufacturer Name": "Apple",
+    	        	"Lead Time": "4",
+    	        	"Description": "Apple MacBook Pro with 13 inch Retina display - Laptop - 3.2 GHz flash storage 2480 x 1680 - 802.11ac",
+    	        	"Price": "1299",
+    	        	"Currency": "USD"
+    	        }
     	      },{
-    	        "title": "Apple MacBook Pro 15\"",
+    	        "title": "Apple MacBook Air",
     	        "imageUrl": "",
-    	        "subtitle": "i7 / 16 GB / 512 GB / Gray",
+    	        "subtitle": "1399 USD",
     	        "buttons": [{
     	            "value": "Buy the third item",
-    	            "title": "$36,000",
+    	            "title": "Order",
     	            "type": "postback"
-    	        }]
+    	        }],
+    	        "details": {
+    	        	"Supplier Name": "REDINGTON DISTRIBUTION PTE LTD",
+    	        	"Supplier Part ID": "AD7399",
+    	        	"Manufacturer Name": "Apple",
+    	        	"Lead Time": "5",
+    	        	"Description": "Apple MacBook Air with 13 inch display - Laptop - 3 GHz flash storage 2480 x 1680 - 802.11ac",
+    	        	"Price": "1399",
+    	        	"Currency": "USD"
+    	        }
     	    }]
     	  },
     	  "delay": null
