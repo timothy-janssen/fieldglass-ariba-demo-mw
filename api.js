9922878ac8d39497c9b125601a0b37fa348a11f0
Timothy Janssen <timothy.janssen@sap.com>
2019-04-25 16:34:16 -0700

    Update api.js

--- api.js
+++ api.js
@@ -40,15 +40,6 @@ exports.call_api_worker_req_create = function(token_data){
 		title = data.jobTitle;
 		code = data.jobCode;
 		status = data.status;
-
-		details = {
-			"Posted by": "Jada Baker",
-           	"Location": "Boston (1710-2017)",
-           	"Start Date": "AUG/01/2018",
-           	"End Date": "AUG/01/2019"
-		}
-		details[title] = code;
-
 		res_data = [{
 			"type": 'text',
 			"content": "Okay. Would you like to submit this requisition request"
@@ -58,7 +49,13 @@ exports.call_api_worker_req_create = function(token_data){
 				"elements": [{
 					"title": "Create Job Posting",
 					"subtitle": title,
-					"details": details
+					"details": {
+						"Job Code": code,
+						"Posted by": "Jada Baker",
+            			"Location": "Boston (1710-2017)",
+            			"Start Date": "AUG/01/2018",
+            			"End Date": "AUG/01/2019"
+					}
 				}]
 			}
 		},{
