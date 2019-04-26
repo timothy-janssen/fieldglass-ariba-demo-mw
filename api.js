32cfb87bbd77701565bd1b945dca3ef52defa31f
Timothy Janssen <timothy.janssen@sap.com>
2019-04-26 12:45:15 -0700

    Update api.js

--- api.js
+++ api.js
@@ -134,10 +134,13 @@ exports.call_api_catalog_search = function(memory){
 	console.log("memory.product: "      + memory.product );
 	console.log("memory.organization: " + memory.organization);
 
-	var query_obj 	= memory.product && true ? memory.product   									  : memory.organization;
-	console.log("query_obj: " + query_obj);
-
-	var query     	= query_obj ? query_obj[0].raw 									  : undefined;
+	var query;
+	if(memory.product && memory.product[0] && true) {
+		query = memory.product[0].raw;
+	} else if (memory.organization && memory.organization[0] && true) {
+		query = memory.organization[0].raw;
+	}
+	
 	console.log("query: " + query);
 	
 	var opts 		= query 		&& true ? "?realm=mytestrealm&rsqlfilter=QueryTerms==" + query : "?realm=mytestrealm";
