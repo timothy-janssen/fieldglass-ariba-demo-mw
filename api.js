a30836a8cba2c371f1a5d3ab4e1616574e9205d6
Timothy Janssen <timothy.janssen@sap.com>
2019-04-26 12:28:52 -0700

    Update api.js

--- api.js
+++ api.js
@@ -131,10 +131,9 @@ const ordinal_values = [
 
 exports.call_api_catalog_search = function(memory){
 
-	var query_obj = memory.product !== undefined ? memory.product   : memory.organization;
-	var query     = query_obj      !== undefined ? query_obj[0].raw : undefined;
-
-	var opts = query ? "?realm=mytestrealm&rsqlfilter=QueryTerms==" + query : "?realm=mytestrealm";
+	var query_obj 	= memory.product !== undefined ? memory.product   									  : memory.organization;
+	var query     	= query_obj      !== undefined ? query_obj[0].raw 									  : undefined;
+	var opts 		= query 		 !== undefined ? "?realm=mytestrealm&rsqlfilter=QueryTerms==" + query : "?realm=mytestrealm";
 
 	console.log("query_obj: " + query_obj);
 	console.log("query: " + query);
