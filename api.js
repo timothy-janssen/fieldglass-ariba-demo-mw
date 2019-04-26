d8148474dafd6bab82a9c1e8c2d368e7a3f9f135
Timothy Janssen <timothy.janssen@sap.com>
2019-04-26 15:09:25 -0700

    Update api.js

--- api.js
+++ api.js
@@ -92,7 +92,7 @@ exports.call_api_worker_req_submit = function(token_data){
 	return request.post(post_options)
 	.then( function(data_) {
 		data = JSON.parse(data_);
-		title = data.jobTitle.replace(/\D/,'');;;
+		title = data.jobTitle.replace(/[0-9]/g, '').replace(/[{()}]/g, '')
 		code = data.jobCode;
 		
 		console.log('[POST] Request completed /job-postings 2')
